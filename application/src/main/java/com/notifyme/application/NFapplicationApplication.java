db2979dc9412ff78679f8a82525725d4f741dd5c
constantin_27@icloud.com <constantin_27@icloud.com>
2023-03-29 08:17:14 +0300

    first commit

---
+++ application/src/main/java/com/notifyme/application/NFapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.notifyme.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NFapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NFapplicationApplication.class, args);
+	}
+
+}
