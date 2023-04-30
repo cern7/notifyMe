f06f3c2de223a1fe644241241f8e3cdaafb33895
cern7 <constantin_27@icloud.com>
2023-04-30 20:11:48 +0300

    commit app start point

--- application/src/main/java/com/notifyme/application/NFApplication.java
+++ application/src/main/java/com/notifyme/application/NFApplication.java
@@ -2,17 +2,18 @@ package com.notifyme.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class NFApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NFApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(NFApplication.class, args);
 
 
 //		String phone = "+40255570090";
 //		String regex ="^\\+40-7[0-9]{2}-[0-9]{3}-[0-9]{3}$";
 //		System.out.println(Pattern.matches(regex, phone));
-	}
+    }
 
 }
