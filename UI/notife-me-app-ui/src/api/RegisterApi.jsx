e7cfd8ee1c67694c8d2c36c3802cc9c1d776b1a2
cer cer <constantin_27@icloud.com>
2023-11-16 07:23:29 +0200

    add header for crossDomain error

--- UI/notife-me-app-ui/src/api/RegisterApi.jsx
+++ UI/notife-me-app-ui/src/api/RegisterApi.jsx
@@ -4,6 +4,7 @@ const BASE_API = `${import.meta.env.VITE_BACKEND_API_URL}/api/auth/register`;
 const postMethodConfig = {
   header: {
     'Content-type': 'application/json; charset=UTF-8',
+    'crossDomain': true
   },
 };
 
