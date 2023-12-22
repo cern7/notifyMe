a0d845e67c104505934c80aec5edbda254a85e7e
const <constantin_27@icloud.com>
2023-12-22 06:59:03 +0200

    Add UI text help for register page

--- UI/notife-me-app-ui/src/api/LoginApi.jsx
+++ UI/notife-me-app-ui/src/api/LoginApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_API = `${import.meta.env.VITE_BACKEND_API_URL}/api/auth/login`;
-console.log(BASE_API);
+
 
 
 const postMethodConfig = {
--- UI/notife-me-app-ui/src/api/RegisterApi.jsx
+++ UI/notife-me-app-ui/src/api/RegisterApi.jsx
@@ -4,8 +4,7 @@ const BASE_API = `${import.meta.env.VITE_BACKEND_API_URL}/api/auth/register`;
 const postMethodConfig = {
   header: {
     'Content-type': 'application/json; charset=UTF-8',
-    'crossDomain': true
-  },
+    },
 };
 
 export const registerNewCustomer = (userDTO) => axios.post(`${BASE_API}`, userDTO, postMethodConfig);
