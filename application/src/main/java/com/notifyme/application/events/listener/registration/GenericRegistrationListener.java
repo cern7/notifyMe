5f5db3bf1525af4ce8f7ab4b652baaedf22e7d4c
Constantin <62252282+cern7@users.noreply.github.com>
2023-12-23 08:11:11 +0200

    add subdomain var

--- application/src/main/java/com/notifyme/application/events/listener/registration/GenericRegistrationListener.java
+++ application/src/main/java/com/notifyme/application/events/listener/registration/GenericRegistrationListener.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 public class GenericRegistrationListener {
     private final RegisterAuthenticationService registerService;
 
+    
     @Value("${domain.url}")
     private String subdomain;
 
